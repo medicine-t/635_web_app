c8acce85afcf21fbb619b18e3fc2d66c987d75d0
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2023-07-20 16:05:56 +0900

    空のモデルの追加

---
+++ hayate/ranking_model.py
@@ -0,0 +1,16 @@
+from model import Model
+
+class RankingModel(Model):
+    def __init__(self):
+        sheets = []
+        super().__init__()
+
+    def setup(self, room_id: str):
+        pass
+
+    def totalling():
+        pass
+
+    def end():
+        pass
+    
---
+++ hayate/standby_model.py
@@ -0,0 +1,11 @@
+from model import Model
+
+class StandbyModel(Model):
+    def setup(self, room_id: str):
+        pass
+
+    def start(self):
+        pass
+
+    def update(self):
+        pass

a89d1959b6865bb709ac4c1d76856dfaa66bdf92
Takuya Takamine <e225717@ie.u-ryukyu.ac.jp>
2023-07-27 14:01:11 +0900

    型の情報を削除

--- api_server/app/main.py
+++ api_server/app/main.py
@@ -18,6 +18,7 @@ class Room(BaseModel):
     host_user : UUID4
     start_time : datetime.time = None
     is_start : bool = False
+    phase_num : int = 0
 
 class Idea(BaseModel):
     idea : str = ""
@@ -27,7 +28,7 @@ class Sheet(BaseModel):
     id: UUID4
     room_id: UUID4
     user_id: UUID4 # writing user id
-    ideas : list[Idea] = [[Idea()] * 3 for i in range(6)]
+    ideas : list[list[Idea]] = [[Idea()] * 3 for _ in range(6)]
 
 rooms:list[Room] = []
 users:list[User] = []
@@ -41,13 +42,21 @@ def index():
 async def get_room_list() -> list[Room]:
     return rooms
 
-@app.post("/v1/room/update/{room_id}")
-async def update_room(room_id:UUID4):
+@app.post("/v1/room/update/")
+async def update_room(room_id:UUID4,user_id:UUID4):
     room:Room = await get_room_info(room_id)
+    if user_id != room.host_user or room.phase_num >= 6:
+        return Response(status_code=403)
+    for s in sheets:
+        for i in range(3):
+            if s.ideas[room.phase_num][i].idea == "":
+                return Response(status_code=403)
+             
     for s in sheets:
         if s.room_id == room_id:
             s.user_id = (room.members.index(s.user_id) + 1) % len(room.members)
-            break
+    room.phase_num += 1
+    return {"message": "success"}
 
 @app.post("/v1/rooms/create/{room_name}/{user_id}")
 async def create_room(room_name:str, user_id:UUID4):
@@ -102,16 +111,20 @@ async def join_room(room_id: UUID4,user_id: UUID4):
     return {"message": "success"}
 
 @app.post("/v1/rooms/{room_id}/start")
-async def start_room(room_id: UUID4) -> datetime.datetime:
+async def start_room(room_id: UUID4,user_id: UUID4) :
     room = await get_room_info(room_id)
+    if room.host_user != user_id:
+        return JSONResponse(status_code=403,content={"message":"you are not host"})
     if type(room) == Response:
         return JSONResponse(status_code=404,content={"message":"room not found"})
     if len(room.members) != 6:
         return JSONResponse(status_code=403,content={"message":"not enough members now"})
+    
     for user in room.members:
         await create_sheet(room_id,user.user_id)
     room.start_time = datetime.datetime.now() + datetime.timedelta(seconds=5)
-    return room.start_time
+    room.is_start = True
+    return {"start_time" : room.start_time}
 
 #@app.post("/v1/sheet/create/{room_id}/{user_id}")
 async def create_sheet(room_id: UUID4,user_id: UUID4):
@@ -127,3 +140,15 @@ async def set_sheet(sheet:Sheet):
             break
     return
 
+@app.get("/v1/sheet/list")
+async def get_sheet_list():
+    return sheets
+
+
+@app.get("/v1/sheet/{room_id}")
+async def get_sheet(room_id: UUID4):
+    ret = []
+    for s in sheets:
+        if s.room_id == room_id:
+            ret.append(s)
+    return ret

30dedbbffc3696d4c12d5f453797948012a779ab
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2023-07-30 23:10:11 +0900

    スタンバイ画面の処理などを追加

--- prototype/mvc_test/controllers.py
+++ prototype/mvc_test/controllers.py
@@ -3,6 +3,10 @@ from views import *
 from frame_switcher import FrameSwitcher
 import tkinter as tk
 
+class Idea(BaseModel):
+    idea : str = ""
+    num_eval : int = 0
+
 class TestController:
     def __init__(self, window: tk.Tk) -> None:
         self.fr = FrameSwitcher(window)
@@ -29,6 +33,8 @@ class Controller:
         self.start_frame = StartFrame(window=window)
         self.registration_frame = RegistrationFrame(window=window)
         self.standby_frame = StandbyFrame(window=window)
+        self.writing_frame = WritingFrame(window=window)
+        self.review_frame = ReviewFrame(window=window)
         self.model = Model()
         self.willCreate = False
 
@@ -36,36 +42,61 @@ class Controller:
         room_list = self.model.getRoomList()
         self.start_frame.setup(room_list)
         self.fr.switchTo(self.start_frame)
-        self.start_frame.CreatRoom.bind("<Button-1>", self.create_room)
+        self.start_frame.CreateRoom.bind("<Button-1>", self.create_room)
         for b in self.start_frame.RoomList:
             b.bind("<Button-1>", self.join_room)
 
     def join_room(self,event: tk.Event):
         button_text = event.widget["text"]
-        room_name = button_text.split(".")[0]
-        room_id = button_text.split(".")[1]
+        self.model.room_name = button_text.split(".")[0]
+        self.model.room_id = button_text.split(".")[1]
         self.willCreate = False
-        self._to_registration(room_name, room_id)
+        self._to_registration()
 
     def create_room(self, event: tk.Event):
-        room_name = self.start_frame.ToNameRoom.get()
-        room_id = ""
+        self.model.room_name = self.start_frame.ToNameRoom.get()
         self.willCreate = True
-        self._to_registration(room_name, room_id)
+        self._to_registration()
 
-    def _to_registration(self, room_name: str, room_id: int):
-        self.registration_frame.setup(room_name)
+    def _to_registration(self):
+        self.registration_frame.setup(self.model.room_name)
         self.fr.switchTo(self.registration_frame)
         self.registration_frame.Registration.bind("<Button-1>", self.registration)
 
     def registration(self, event: tk.Event):
-        user_name = self.registration_frame.UserName.get()
+        user_name = self.registration_frame.UserName.cget("text")
         self.model.createUser(user_name)
-        room_name = self.registration_frame.RoomName.cget("text")
 
         if self.willCreate:
-            self.model.createRoom(room_name,self.model.user_id)
+            self.model.createRoom()
 
-        self.model.registration(self.model.room_id,self.model.user_id)
+        self.model.registration()
 
-        ## self._to~~
+        self._to_standby()
+
+    def _to_standby(self):
+        room = self.model.getRoom()
+        self.standby_frame.setup(room)
+        self.standby_update()
+    
+    def standby_update(self):
+        room = self.model.getRoom()
+        if room["is_start"] == True:
+            print("room is start")
+        self.standby_frame.after(1000, self.standby_update)
+
+    def _to_writing(self):
+        room = self.model.getRoom()
+        sheet = self.model.getSheet()
+        self.writing_frame.setup(room, sheet["ideas"], self.model.writing_count)
+    
+    def writing_update(self):
+        sheet = self.model.getSheet()
+        ideaTextBox = self.writing_frame.ideaTextBox
+        ideas = []
+        for textbox in ideaTextBox:
+            idea = Idea()
+            idea.idea = textbox.get()
+            ideas.append(idea)
+        sheet["ideas"] = ideas
+        self.model.setSheet(sheet)

42c8c7a496618a272bb747efc6293bc0600e9f85
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2023-07-30 23:10:31 +0900

    モデル側で、いろいろな情報をメソッドとして保持管理するよう変更

--- prototype/mvc_test/models.py
+++ prototype/mvc_test/models.py
@@ -3,9 +3,12 @@ import datetime
         
 class Model:
     def __init__(self) -> None:
-        self.url = "http://10.10.0.53" # プログラマが設定
+        self.url = "http://localhost" # プログラマが設定
         self.room_id = "" #Controllerの_to_registrationでroom_idを設定する。
         self.user_id = "" #部屋参加登録時に更新
+        self.room_name = ""
+        self.writing_count = 0
+        self.review_count = 0
 
     ### Room ###
     def getRoomList(self):
@@ -13,20 +16,21 @@ class Model:
         room_list = response.json()
         return room_list
     
-    def getRoom(self, room_id):
-        response = requests.get(f"{self.url}/v1/rooms/{room_id}")
+    def getRoom(self):
+        response = requests.get(f"{self.url}/v1/rooms/{self.room_id}")
         room = response.json()
         return room
     
-    def createRoom(self,room_name:str, user_id):
-        response = requests.post(f"{self.url}/v1/rooms/create/{room_name}/{user_id}")
+    def createRoom(self):
+        response = requests.post(f"{self.url}/v1/rooms/create/{self.room_name}/{self.user_id}")
         if response.status_code != 200:
             return False
-        return response.json()["room_id"]
+        self.room_id = response.json()["room_id"]
+        return True
     
 
-    def updateRoom(self,room_id,user_id) :
-        response = requests.post(f"{self.url}/v1/room/update/",params={"user_id":user_id,room_id:room_id})
+    def updateRoom(self) :
+        response = requests.post(f"{self.url}/v1/room/update/",params={"user_id":self.user_id,"room_id":self.room_id})
         if response.status_code != 200:
             return False
         return True
@@ -38,30 +42,38 @@ class Model:
         return response.json()["start_time"]
 
     ### User ###
-    def createUser(self,user_name:str):
+    def createUser(self, user_name: str):
         response = requests.post(f"{self.url}/v1/users/create/",params={"user_name" : user_name})
         if response.status_code != 200:
             return False 
         self.user_id = response.json()["user_id"]
         return True
     
-    def registration(self, room_id, user_id):
-        response = requests.post(f"{self.url}/v1/rooms/{room_id}/join",params={"user_id":user_id})
+    def registration(self):
+        response = requests.post(f"{self.url}/v1/rooms/{self.room_id}/join",params={"user_id":self.user_id})
         if response.status_code != 200:
             return False
-        
-        self.room_id = room_id
         return True
     
     ### Sheet ###
 
-    def getSheetList(self, room_id):
-        response = requests.get(f"{self.url}/v1/sheet/{room_id}")
+    def getSheetList(self):
+        response = requests.get(f"{self.url}/v1/sheet/{self.room_id}")
         if response.status_code != 200:
             return False
         return response.json()
     
-    def setSheet(self,sheet):
+    def getSheet(self):
+        sheets = self.getSheetList(self.room_id)
+        sheet = None
+        for s in sheets:
+            if s["user_id"] == self.user_id:
+                sheet = s
+                break
+
+        return sheet
+    
+    def setSheet(self, sheet):
         response = requests.post(f"{self.url}/v1/sheet/update",json=sheet.dict())
         if response.status_code != 200:
             return False

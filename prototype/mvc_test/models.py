262fbadf23746703d5ce6ade75f1f09c88829753
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2023-08-01 13:38:00 +0900

    setSheet urlミス修正&sheet作成処理追加

--- prototype/mvc_test/models.py
+++ prototype/mvc_test/models.py
@@ -1,5 +1,9 @@
 import requests
 import datetime
+
+class Idea():
+    idea : str = ""
+    num_eval : int = 0
         
 class Model:
     def __init__(self) -> None:
@@ -7,8 +11,7 @@ class Model:
         self.room_id = "" #Controllerの_to_registrationでroom_idを設定する。
         self.user_id = "" #部屋参加登録時に更新
         self.room_name = ""
-        self.writing_count = 0
-        self.review_count = 0
+        self.phase_num = 0
 
     ### Room ###
     def getRoomList(self):
@@ -36,8 +39,8 @@ class Model:
             return False
         return True
     
-    def startRoom(self,room_id,user_id) -> datetime.datetime:  
-        response = requests.post(f"{self.url}/v1/rooms/{room_id}/start",params={"user_id":user_id})
+    def startRoom(self) -> datetime.datetime:  
+        response = requests.post(f"{self.url}/v1/rooms/{self.room_id}/start",params={"user_id":self.user_id})
         if response.status_code != 200:
             return False
         return True
@@ -65,7 +68,7 @@ class Model:
         return response.json()
     
     def getSheet(self):
-        sheets = self.getSheetList(self.room_id)
+        sheets = self.getSheetList()
         sheet = None
         for s in sheets:
             if s["user_id"] == self.user_id:
@@ -74,15 +77,16 @@ class Model:
 
         return sheet
     
-    def setSheet(self, sheet):
-        response = requests.post(f"{self.url}/v1/sheet/update",json=sheet.dict())
+    def setSheet(self, idea_texts):
+        ideas = []
+        for text in idea_texts:
+            idea = {"idea": text, "num_eval": 0}
+            ideas.append(idea)
+
+        sheet = self.getSheet()
+        sheet["ideas"][self.phase_num] = ideas
+
+        response = requests.post(f"{self.url}/v1/sheet/update/",json=sheet)
         if response.status_code != 200:
             return False
         return True
-    
-class TestModel:
-    def test(self, text):
-        print(text)
-
-    def waitng_print(self):
-        print("I'm wainting!")
